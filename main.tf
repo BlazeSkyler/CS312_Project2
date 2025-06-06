a05a9fa82e968b3930acf41d9721d45a102f8b48
BlazeSkyler <ucols@oregonstate.edu>
2025-06-05 19:34:04 -0700

    new changes that were missed

--- main.tf
+++ main.tf
@@ -44,18 +44,18 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
 
 resource "aws_vpc_security_group_ingress_rule" "https" {
   security_group_id = aws_security_group.mc_sec_group.id
-  from_port = 443
-  to_port = 443
-  ip_protocol = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "mc_port" {
   security_group_id = aws_security_group.mc_sec_group.id
-  from_port = 25565
-  to_port = 25565
-  ip_protocol = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
+  from_port         = 25565
+  to_port           = 25565
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_egress_rule" "all_outbound" {
@@ -66,11 +66,24 @@ resource "aws_vpc_security_group_egress_rule" "all_outbound" {
 
 resource "aws_instance" "mc_server" {
   ami                    = "ami-0a605bc2ef5707a18"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.mc_sec_group.id]
   key_name               = aws_key_pair.pub_key.key_name
 
   tags = {
     Name = "Minecraft_Server_2"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host = "${self.public_dns}"
+      user = "ubuntu"
+      type = "ssh"
+      private_key = "${file("~/.ssh/id_rsa")}"
+      timeout = "2m"
+    }
+    inline = [
+      "./${file("./docker_install.sh")}"
+      ]
   }
 }
