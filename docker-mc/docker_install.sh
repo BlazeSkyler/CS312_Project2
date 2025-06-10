2d2aa732c1725876fe496fd4dde6ec0139dae68e
BlazeSkyler <ucols@oregonstate.edu>
2025-06-10 15:37:27 -0700

    removed comments

--- docker-mc/docker_install.sh
+++ docker-mc/docker_install.sh
@@ -2,8 +2,6 @@
 
 sudo apt update && sudo apt -y upgrade
 
-# Docker installation
-# Set up apt repo
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 sudo apt-get update
 sudo apt-get install ca-certificates curl
